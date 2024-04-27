b592897608b0b79d27c1ccd57c7b190cc5a5a6d8
Joey Meere <100378695+joeymeere@users.noreply.github.com>
2024-04-27 12:59:21 -0400

    ---

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,7 @@ import { Connection } from "@solana/web3.js";
 
 describe("getIDL", async () => {
     const a = await getIDL(
-        new Connection("https://mainnet.helius-rpc.com/?api-key=6d192cd3-b66b-4bf8-b9b6-2f731bb72fe0"), 
+        new Connection("https://mainnet.helius-rpc.com/?api-key=<key>"), 
         "STKUaKniasuqrfer3XNbmrrc578pkL1XACdK8H3YPu8"
       );
     
